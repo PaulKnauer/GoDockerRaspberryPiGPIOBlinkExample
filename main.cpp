8c7cce7afbfb50f538d6ea6d3f8a33c313d79950
Paul Knauer <paul.knauer@gmail.com>
2016-11-15 15:20:52 +0000

    Removed stdio

--- main.cpp
+++ main.cpp
@@ -1,22 +1,16 @@
-#include <iostream>
 #include <wiringPi.h>
 
 #include "i2c.h"
 
-using namespace std;
-
 int main() {
   wiringPiSetup();
   pinMode(0, OUTPUT);
-
-  cout << "Set up GPIO" << endl;
 
   for(;;) {
     digitalWrite(0, HIGH);
     delay(500);
     digitalWrite(0,  LOW);
     delay(500);
-    cout << "Blink" << endl;
   }
 
   return 0;
